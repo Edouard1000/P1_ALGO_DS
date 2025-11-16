7f3c654daecab6e8819b0af980918fc56b7fe5e4
Edouard Meurant <edouard@MacBookPro.fritz.box>
2025-11-16 17:04:08 +0100

    enlever les deepcopy

--- reverse_vectorized.jl
+++ reverse_vectorized.jl
@@ -11,19 +11,21 @@ mutable struct VectNode
     value::Union{Float64, Array{Float64}}
     derivative::Union{Nothing, Float64, Array{Float64}} # dl/df  #Jv(L)
 	forward_derivative::Union{Nothing, Float64, Array{Float64}} #df/dx 
+    second_order_derivative::Union{Nothing, Float64, Array{Float64}} #Hv(L)
 	memory::Union{Nothing, Float64, Tuple{AbstractArray, AbstractArray}, AbstractArray, Vector{Int}}
 	need_grad::Bool
 	need_hess::Bool
 end
 # -------------------- init -------------------- #
-function VectNode(op, args, value, need_grad = true, need_hess = false, f_grad = nothing)
+function VectNode(op, args, value, need_grad = true, need_hess = true, f_grad = nothing)
     v = _make_value(value)
 	grad = (need_grad ? zeros_like(v) : nothing)
+    second_order_derivative = (need_hess ? zeros_like(v) : nothing)
 	f_gradi = isnothing(f_grad) ? nothing : _make_value(f_grad) 
-	return VectNode(op, args, v, grad, f_gradi, nothing, need_grad, need_hess)
+	return VectNode(op, args, v, grad, f_gradi, second_order_derivative, nothing, need_grad, need_hess)
 end
-VectNode(x::Number, need_grad = true, need_hess = false, f_grad = nothing) = VectNode(nothing, VectNode[], x, need_grad, need_hess, f_grad)
-VectNode(x::AbstractArray, need_grad = true, need_hess = false, f_grad = nothing) = VectNode(nothing, VectNode[], x, need_grad, need_hess, f_grad)
+VectNode(x::Number, need_grad = true, need_hess = true, f_grad = nothing) = VectNode(nothing, VectNode[], x, need_grad, need_hess, f_grad)
+VectNode(x::AbstractArray, need_grad = true, need_hess = true, f_grad = nothing) = VectNode(nothing, VectNode[], x, need_grad, need_hess, f_grad)
 
 # -------------------- static functions -------------------- #
 _make_value(x::Union{Number, AbstractArray}) = isa(x, Number) ? Float64(x) : convert.(Float64, x)
@@ -130,13 +132,13 @@ check_param[".*"] = (sx, sy) -> begin
 	if sx != () && sy != () && (sx != sy) error("multiplication broadcasting is not implemented for $sx and $sy") end
 end
 check_param["sum"] = (sx) -> begin
-	if length(sx) != 2 error("sum not define for $sx") end
+	return
 end
 check_param["sum_all"] = (sx) -> begin
 	return
 end
 check_param["maximum"] = (sx) -> begin
-	if length(sx) != 2 error("maximum along a dim is not implemented for $sx") end
+    return
 end
 check_param["maximum_all"] = (sx) -> begin
 	return
@@ -222,16 +224,16 @@ local_back_rule["maximum_all"] = (f) -> begin
 	x.derivative[idx] += f.derivative
 end
 local_back_rule["./"] = (f) -> begin
-	x = f.args[1] ; z = f.args[2]; sx = shape(x); sz = shape(z)
-	if shape(x) != shape(z) && !isscalar(x) && !isscalar(z)
-		if isnothing(f.memory) f.memory = (extend(z.value, shape(x)), z.value .^ 2) end
-		if x.need_grad x.derivative .+= f.derivative ./ f.memory[1] end
-		if z.need_grad z.derivative .-= compute_diag_prod(x.value, f.derivative') ./ f.memory[2] end
-	else
-		if isnothing(f.memory) f.memory = x.value ./ (z.value .^2) end
-		if x.need_grad x.derivative = x.derivative .+ f.derivative ./ z.value end
-		if z.need_grad z.derivative = z.derivative .- f.derivative .* f.memory end
-	end
+    x = f.args[1] ; z = f.args[2]; sx = shape(x); sz = shape(z)
+    if shape(x) != shape(z) && !isscalar(x) && !isscalar(z)
+        if isnothing(f.memory) f.memory = (extend(z.value, shape(x)), z.value .^ 2) end
+        if x.need_grad x.derivative .+= f.derivative ./ f.memory[1] end
+        if z.need_grad z.derivative .-= compress(x.value .* f.derivative, sz) ./ f.memory[2] end
+    else
+        if isnothing(f.memory) f.memory = x.value ./ (z.value .^2) end
+        if x.need_grad x.derivative = x.derivative .+ f.derivative ./ z.value end
+        if z.need_grad z.derivative = z.derivative .- f.derivative .* f.memory end
+    end
 end
 local_back_rule[".-"] = (f) -> begin
 	x = f.args[1]; z = f.args[2]; sx = shape(x); sz = shape(z)
@@ -370,6 +372,214 @@ local_forw_rule[".^"] = (f) -> begin
     x = f.args[1]; n = f.args[2].value
     f.forward_derivative = (n .* (x.value .^ (n .- 1))) .* x.forward_derivative
 end
+local_forw_over_rev_rule = Dict{String, Function}()
+
+local_forw_over_rev_rule["*"] = (f) -> begin
+    x, y = f.args
+    if x.need_hess
+        x.second_order_derivative .+= f.second_order_derivative * y.value' .+ f.derivative * y.forward_derivative'
+    end
+    if y.need_hess
+        y.second_order_derivative .+= x.value' * f.second_order_derivative .+ x.forward_derivative' * f.derivative
+    end
+end
+
+local_forw_over_rev_rule[".*"] = (f) -> begin
+    x, y = f.args
+    if x.need_hess
+        x.second_order_derivative .+= f.second_order_derivative .* y.value .+ f.derivative .* y.forward_derivative
+    end
+    if y.need_hess
+        y.second_order_derivative .+= f.second_order_derivative .* x.value .+ f.derivative .* x.forward_derivative
+    end
+end
+
+local_forw_over_rev_rule[".+"] = (f) -> begin
+    x, y = f.args
+    
+    if isscalar(f)
+        if x.need_hess
+            x.second_order_derivative += f.second_order_derivative
+        end
+        if y.need_hess
+            y.second_order_derivative += f.second_order_derivative
+        end
+    else
+        if x.need_hess
+            x.second_order_derivative .+= f.second_order_derivative
+        end
+        if y.need_hess
+            y.second_order_derivative .+= f.second_order_derivative
+        end
+    end
+end
+
+local_forw_over_rev_rule[".-"] = (f) -> begin
+    x, y = f.args
+    
+    if isscalar(f)
+        if x.need_hess
+            x.second_order_derivative += f.second_order_derivative
+        end
+        if y.need_hess
+            y.second_order_derivative -= f.second_order_derivative
+        end
+    else
+        if x.need_hess
+            x.second_order_derivative .+= f.second_order_derivative
+        end
+        if y.need_hess
+            y.second_order_derivative .-= compress(f.second_order_derivative, shape(y))
+        end
+    end
+end
+
+local_forw_over_rev_rule["./"] = (f) -> begin
+    x, z = f.args
+    
+    if isscalar(f)
+        
+        z_val = z.value
+        z2 = z_val^2
+        
+        if x.need_hess
+            # Pas de '.+=' pour un scalaire !
+            x.second_order_derivative += f.second_order_derivative / z_val - f.derivative * (z.forward_derivative / z2)
+        end
+        
+        if z.need_hess
+            x_val = x.value
+            t1 = -f.second_order_derivative * (x_val / z2)
+            
+            t2_num = x.forward_derivative * z2 - 2.0 * x_val * z_val * z.forward_derivative
+            t2_den = z2^2 # (z_val^2)^2 == z_val^4. t2_den = z2^2 est correct.
+            t2 = -f.derivative * (t2_num / t2_den)
+            
+            # Pas de '.+=' pour un scalaire !
+            z.second_order_derivative += t1 + t2
+        end
+        
+    else
+ 
+        z2 = z.value .^ 2
+        
+        if x.need_hess
+            x.second_order_derivative .+= f.second_order_derivative ./ z.value .- f.derivative .* (z.forward_derivative ./ z2)
+        end
+        
+        if z.need_hess
+            t1 = -f.second_order_derivative .* (x.value ./ z2)
+            t2_num = z2 .* x.forward_derivative .- 2.0 .* x.value .* z.value .* z.forward_derivative
+            t2 = -f.derivative .* (t2_num ./ (z2 .^ 2)) 
+            
+            z.second_order_derivative .+= compress(t1 .+ t2, shape(z))
+        end
+    end
+end
+
+local_forw_over_rev_rule["sum"] = (f) -> begin
+    x = f.args[1]
+    if x.need_hess
+        x.second_order_derivative .+= extend(f.second_order_derivative, shape(x))
+    end
+end
+
+local_forw_over_rev_rule["sum_all"] = (f) -> begin
+    x = f.args[1]
+    if x.need_hess
+        x.second_order_derivative .+= extend(f.second_order_derivative, size(x.value))
+    end
+end
+local_forw_over_rev_rule[".exp"] = (f) -> begin
+    x = f.args[1]
+    e = exp.(x.value)
+    if x.need_hess
+        x.second_order_derivative .+= f.second_order_derivative .* e .+ f.derivative .* (e .* x.forward_derivative)
+    end
+end
+
+local_forw_over_rev_rule[".log"] = (f) -> begin
+    x = f.args[1]
+    if x.need_hess
+        x.second_order_derivative .+= f.second_order_derivative ./ x.value .+ f.derivative .* ((-1.0 ./ (x.value.^2)) .* x.forward_derivative)
+    end
+end
+
+local_forw_over_rev_rule[".tanh"] = (f) -> begin
+    x = f.args[1]
+    t = tanh.(x.value)
+    s = 1 .- t.^2  
+    if x.need_hess
+        x.second_order_derivative .+= f.second_order_derivative .* s .+
+                                 f.derivative .* ((-2 .* t .* s) .* x.forward_derivative)
+    end
+end
+
+
+local_forw_over_rev_rule[".relu"] = (f) -> begin
+    x = f.args[1]
+    
+    if x.need_hess
+        mask = isnothing(f.memory) ? (x.value .> 0) : f.memory
+
+        if isscalar(f)
+            x.second_order_derivative += f.second_order_derivative * mask
+        else
+            x.second_order_derivative .+= f.second_order_derivative .* mask
+        end
+    end
+end
+
+local_forw_over_rev_rule[".^"] = (f) -> begin
+    x = f.args[1]
+    n = f.args[2].value
+    if x.need_hess
+        xn1 = x.value .^ (n - 1)
+        xn2 = (n > 1) ? x.value .^ (n - 2) : ones_like(x)
+        
+        t1 = f.second_order_derivative .* (n .* xn1) 
+        t2 = f.derivative .* (n*(n-1) .* xn2 .* x.forward_derivative) 
+        
+        x.second_order_derivative .+= t1 .+ t2
+    end
+end
+
+local_forw_over_rev_rule["maximum"] = (f) -> begin
+    x = f.args[1]
+    sx, sf = shape(x), shape(f)
+
+
+    if x.need_hess
+        if sf[1] == 1
+            for j in 1:sx[2]
+                idx = f.memory[j]
+                
+                x.second_order_derivative[idx, j] += f.second_order_derivative[1, j]
+            end
+        else
+            for i in 1:sx[1]
+                idx = f.memory[i]
+
+                x.second_order_derivative[i, idx] += f.second_order_derivative[i, 1]
+            end
+        end
+    end
+end
+
+
+local_forw_over_rev_rule["maximum_all"] = (f) -> begin
+    x = f.args[1]
+    if isnothing(f.memory)
+        f.memory = argmax(x.value)
+    end
+    
+    if x.need_hess
+        idx = f.memory
+        x.second_order_derivative[idx] += f.second_order_derivative
+    end
+end
+
+
 
 
 # ............... broadcasting ............... #
@@ -477,7 +687,9 @@ end
 
 function forward!(f::VectNode)
     if isempty(f.args)
-        f.forward_derivative = zeros_like(f)
+        if isnothing(f.forward_derivative)
+            f.forward_derivative = zeros_like(f)
+        end
         return
     end
     for arg in f.args
@@ -488,6 +700,20 @@ function forward!(f::VectNode)
     local_forw_rule[f.op](f)
 end
 
+function second_order_backward(f::VectNode)
+    
+    visited = Set{VectNode}()
+    topo = VectNode[]
+    topo_sort!(visited, topo, f)  
+    reverse!(topo)                 
+
+    for node in topo
+        if !isnothing(node.op)
+            local_forw_over_rev_rule[node.op](node)
+        end
+    end
+    return f
+end
 
 function topo_sort!(visited::Set{VectNode}, topo::Vector{VectNode}, f::VectNode)
     if !(f in visited)
@@ -517,12 +743,37 @@ function backward!(f::VectNode)
     return f
 end
 
-function hess(f, x::Flatten, dir::Flatten)
-	x_nodes = Flatten([VectNode(x.components[i], true, true, dir.components[i]) for i in 1: length(x.components)])
-	last_node = f(x_nodes)
-	forward!(last_node)
-	backward!(last_node)
-	#on doit encore appliqué la big formule !
+function hvp(f, x, v)
+    x_nodes = Flatten([VectNode(xi, true, true, vi) for (xi, vi) in zip(x.components, v.components)])
+    last_node = f(x_nodes)
+    
+    forward!(last_node)
+
+    backward!(last_node)    
+            
+    second_order_backward(last_node)  
+
+    hv = zero(v)
+    for i in eachindex(x.components)
+        hv.components[i] .= x_nodes.components[i].second_order_derivative
+    end
+    return hv
+end
+
+function hessian(f, x::Flatten)
+    n = length(x)
+    H = zeros(n, n)
+    basis = zero(x)
+
+    for i in 1:n
+        v = zero(basis)
+        v[i] = 1.0
+
+        hv = hvp(f, x, v)
+        H[:, i] .= reduce(vcat, vec.(hv.components))
+    end
+
+    return H
 end
 
 function slow_grad!(f, g::Flatten, x::Flatten)
@@ -571,8 +822,8 @@ function gradient!(f, g::Flatten, x::Flatten)
 end
 
 gradient(f, x) = begin
-	g = deepcopy(x)
-	slow_grad!(f, g, x)
+	g = zero(x)
+	gradient!(f, g, x)
 end
 
 end
